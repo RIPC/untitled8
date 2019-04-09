525f273c72c3a84fcce0aab3415b2cb85a303d5a
loveandpepper <loveandpepper@gmail.com>
2019-04-09 12:23:28 +0300

    nothing has been done

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        Message m = new Message();
+        String lalkinshake = "lalkinshake";
+        m.ptintMsg(lalkinshake);
+        Thread.sleep(500);
+        System.out.println("Now something has been done");
+    }
+}
+
